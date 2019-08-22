fdb0ea27581ec84253147f91c3d547097cefdd19
Anderson Jimenez <anderson.jimenez@farleyetc.com>
2019-08-22 17:31:29 -0500

    creando react redux

---
+++ citasredux/src/components/ListadoCitas.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ListadoCitas() {
+
+    return (
+        <div className="card mt-5">
+            <div className="card-body">
+                <h2 className="card-title text-center">
+                    Administra las Citas Aquí
+                </h2>
+            </div>
+        </div>
+    )
+
+}
+
+export default ListadoCitas;
